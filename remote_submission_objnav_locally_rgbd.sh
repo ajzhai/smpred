6f81b1dc6016a0ba9d471602572eb4960d7c4134
ajzhai <a.jianqiao.zhai@gmail.com>
2022-07-09 19:24:41 -0500

    docker gpu

--- remote_submission_objnav_locally_rgbd.sh
+++ remote_submission_objnav_locally_rgbd.sh
@@ -21,7 +21,7 @@ done
 
 docker run -v $(pwd)/habitat-challenge-data:/habitat-challenge-data \
     -v $(pwd)/data:/data\
-    --gpus=all \
+    --gpus='"device=8"' \
     -e "AGENT_EVALUATION_TYPE=local" \
     -e "TRACK_CONFIG_FILE=/challenge_objectnav2021.local.rgbd.yaml" \
     ${DOCKER_NAME}\
