1b7fb46d75eca4f1e8355ccf1a21fcfdc4caea96
ajzhai <a.jianqiao.zhai@gmail.com>
2022-07-10 14:09:21 -0500

    collect script

---
+++ Stubborn/collect.py
@@ -0,0 +1,38 @@
+import argparse
+import os
+import random
+import habitat
+import torch
+from arguments import get_args
+from habitat.core.env import Env
+import numpy as np
+
+from agent.stubborn_agent import StubbornAgent
+
+def main():
+
+    args_2 = get_args()
+    args_2.sem_gpu_id = 0
+    config_paths = os.environ["CHALLENGE_CONFIG_FILE"]
+    config = habitat.get_config(config_paths)
+    nav_agent = StubbornAgent(args=args_2,task_config=config)
+    hab_env = Env(config=config_env)
+    
+    num_episodes = len(self._env.episodes)
+    print(num_episodes)
+    exit(0)
+    count_episodes = 0
+    while count_episodes < num_episodes:
+        observations = hab_env.reset()
+        nav_agent.reset()
+
+        while not hab_env.episode_over:
+            action = nav_agent.act(observations)
+            observations = hab_env.step(action)
+
+        count_episodes += 1
+
+    
+
+if __name__ == "__main__":
+    main()
