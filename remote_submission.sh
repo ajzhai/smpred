b1b08b507bab04f7b2e6f4d11289a6135c7a47e5
ajzhai <a.jianqiao.zhai@gmail.com>
2022-07-25 20:15:44 -0500

    parallel collect fix

--- remote_submission.sh
+++ remote_submission.sh
@@ -2,6 +2,6 @@
 
 # python Stubborn/collect.py -v 0 --dump_location ./data/tmp --print_images 1 --timestep_limit 1000 --evaluation $AGENT_EVALUATION_TYPE $@
 
-python Stubborn/collect.py --sem_gpu_id 2 --start_ep 0 --end_ep 2 --timestep_limit 1000 --evaluation $AGENT_EVALUATION_TYPE $@ > collectlog.txt &
-python Stubborn/collect.py --sem_gpu_id 3 --start_ep 2 --end_ep 4 --timestep_limit 1000 --evaluation $AGENT_EVALUATION_TYPE $@ > collectlog.txt &
+python Stubborn/collect.py --sem_gpu_id 2 --start_ep 0 --end_ep 2 --timestep_limit 1000 --evaluation $AGENT_EVALUATION_TYPE $@  &
+python Stubborn/collect.py --sem_gpu_id 3 --start_ep 2 --end_ep 4 --timestep_limit 1000 --evaluation $AGENT_EVALUATION_TYPE $@  &
 wait
