d56b89f9de321024de6ff17e1f455e17d1bb7ee9
ajzhai <a.jianqiao.zhai@gmail.com>
2022-08-30 01:46:28 -0500

    default submission

--- submission.sh
+++ submission.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Switch step: explore -> smp
-python Stubborn/eval.py -v 0 --sem_pred_prob_thr 0.9 --smp_step 10 --switch_step 19 --alpha 400 --col_rad 4 --num_sem_categories 23 --global_downscaling 3 --map_size_cm 4800  --evaluation remote $@
+python Stubborn/eval.py -v 0 --sem_pred_prob_thr 0.9 --smp_step 10 --switch_step 19 --alpha 400 --col_rad 4 --global_downscaling 3 --map_size_cm 4800  --evaluation remote $@
