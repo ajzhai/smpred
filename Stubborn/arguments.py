29c89489ec66da876e10f7d35d867d107e0dfc36
ajzhai <a.jianqiao.zhai@gmail.com>
2022-10-03 17:05:01 -0500

    stair and escape

--- Stubborn/arguments.py
+++ Stubborn/arguments.py
@@ -112,6 +112,7 @@ def get_args():
     parser.add_argument('--oventrick', type = int, default = 0)
     parser.add_argument('--toiletgrow', type = int, default = 0)
     parser.add_argument('--toiletrad', type = int, default = 6)
+    parser.add_argument('--stair_thr', type = float, default = 0.2)
                             
     # for data collection purposes. Use 0 to turn off
     # use 1 to turn on
