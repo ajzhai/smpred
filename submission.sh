837cb1daa1b0c1b635eb76264cd43c84de652b03
ajzhai <a.jianqiao.zhai@gmail.com>
2022-10-31 16:48:18 -0500

    hm3d teststd

--- submission.sh
+++ submission.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Switch step: explore -> smp
-python Stubborn/eval.py -v 0 --inhib_mode 2 --num_sem_categories 16 --dd_erode 1 --evaluation remote $@
+python Stubborn/eval.py -v 0 --inhib_mode 2  --dd_erode 1 --evaluation remote $@
