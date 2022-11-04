f158f935c49b398da071897008de68062db9eabb
ajzhai <a.jianqiao.zhai@gmail.com>
2022-11-04 12:14:04 -0500

    mp3d test again

--- submission.sh
+++ submission.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Switch step: explore -> smp
-python Stubborn/eval.py -v 0 --inhib_mode 2  --dd_erode 1 --evaluation remote $@
+python Stubborn/eval.py -v 0 --inhib_mode 0  --alpha 100 --erode_recover 1 --num_sem_categories 23 --evaluation remote $@
